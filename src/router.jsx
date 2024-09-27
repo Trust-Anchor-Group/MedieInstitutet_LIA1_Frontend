144dc5c4bd8a9509df5f3d824bdba1b94e05c98d
Christian Lopez <chrisdev92@outlook.com>
2024-09-27 13:39:14 +0200

    setting the home component as default route for the application

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 // Creating a router configuration using createBrowserRouter
 export const Router = createBrowserRouter([
@@ -10,6 +11,10 @@ export const Router = createBrowserRouter([
         element: <Layout/>, //Component to render at the root path
         
         children: [ //Nested routes
+            {
+                index: true, 
+                element: <Home/> 
+            },
             {
                 path: "/register", //Path for the Register component
                 element: <Register/> //Component to render at the /register path
