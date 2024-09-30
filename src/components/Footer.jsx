e66cd856211aafed4611d983efa52b598d2a74d5
Christian Lopez <chrisdev92@outlook.com>
2024-09-30 09:37:58 +0200

    footer content with dummy data for social media

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <ul>
+          <li className="center-content">&copy; 2024 | All rights reserved</li>
+          <li className="social-media">
+            <ul>
+              <li>Social Media Link 1</li>
+              <li>Social Media Link 2</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer
