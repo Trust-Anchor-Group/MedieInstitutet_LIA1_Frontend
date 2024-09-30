6e444df3ea32212bced4b22b05775e3097a68c73
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-09-30 19:58:45 +0200

    :lipstick: ui: add new banner component
    - add new styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Banner } from '../components/Banner';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Banner imageSource="/public/assets/images/hero.jpg">
+        <div className="banner__promo-card">
+          <h1>
+            Forget about programmers — creating contracts has never been easier!
+          </h1>
+        </div>
+      </Banner>
+    </>
+  );
+};
 
-export default Home
+export default Home;
