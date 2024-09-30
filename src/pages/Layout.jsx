465652b22c364aae6ff8321ef913677cb81ccd4e
Christian Lopez <chrisdev92@outlook.com>
2024-09-30 09:45:53 +0200

    positioning the footer and basic styling

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Layout = () => {
   return (
@@ -11,6 +12,9 @@ const Layout = () => {
     <Outlet/>
 
     </main>
+    
+    <Footer/>
+    
     </>
   )
 }
