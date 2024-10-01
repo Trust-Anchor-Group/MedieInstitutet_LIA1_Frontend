73de3fcd564505e175a80dd1cce2bdff96bf7c13
Christian Lopez <chrisdev92@outlook.com>
2024-10-01 08:50:50 +0200

    Implemented RegistrationForm component with state management and API integration

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import httpService from '../services/httpService'
+
+const RegisterForm = () => {
+    const [formData, setFormData] = useState({
+      UserName: '',
+      EMail: '',
+      Password: '',
+    });
+
+    // Function to handle changes in the input fields of the form.
+    const handleChange = (e) => {
+      // Destructure name and value from the event target (input field)
+      const { name, value } = e.target;
+      
+      // Update the formData state with the new value for the corresponding input field
+      setFormData({
+        ...formData, // Spread the existing formData
+        [name]: value // Update the specific field with the new value
+      });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await httpService.registerUser(formData);
+            console.log("Registration successful", response);
+        } catch(error){
+            console.log("Registration failed", error);
+        }
+    }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            name="UserName"
+            type="text"
+            value={formData.UserName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            name="EMail"
+            type="email"
+            value={formData.EMail}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            name="Password"
+            type="password"
+            value={formData.Password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+}
+
+export default RegisterForm
