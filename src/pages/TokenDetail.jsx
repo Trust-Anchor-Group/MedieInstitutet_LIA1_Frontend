c2f15e81126e5cde6acbb548a609862451f0c09a
Christian Lopez <chrisdev92@outlook.com>
2024-10-11 16:37:10 +0200

    working on styling and logic for tokendetail

--- src/pages/TokenDetail.jsx
+++ src/pages/TokenDetail.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import tokenData from '../data/tokenData'; 
+import ManageToken from '../components/ManageToken';
 
 const TokenDetail = () => {
+  const [tokens, setTokens] = useState(tokenData);
+  const[editingTokenId, setEditingTokenId] = useState(null);
   const [expandedTokenId, setExpandedTokenId] = useState(null);
 
+  //Toggle the token details visibility, if open close it and vice versa
   const toggleDetails = (tokenId) => {
     setExpandedTokenId(expandedTokenId === tokenId ? null : tokenId );
   };
 
+  const handleSave = (updatedToken) => {
+    console.log("updatedToken:", updatedToken);
+    
+    setTokens(
+      tokens.map((token) =>
+        token.tokenId === updatedToken.tokenId ? updatedToken : token
+      )
+    );
+    console.log("tokens:", tokens);
+    
+    setEditingTokenId(null);
+  };
+
   return (
     <div className="tokenList">
-      {tokenData.map((token) => (
+      {tokens.map((token) => (
         <div
           key={token.tokenId}
           className="tokenContainer"
@@ -27,11 +44,20 @@ const TokenDetail = () => {
               <p>Token_Id: {token.tokenId}</p>
               <p>Name: {token.name}</p>
               <p>Symbol: {token.symbol}</p>
-              <p>Total Supply: {token.totalSupply}</p>
+              <p>Quantity: {token.quantity}</p>
               <p>Price: {token.price}</p>
               <p>Currency: {token.currency}</p>
-              <p>Amount For Sale: {token.listedForSale}</p>
-              <p>Unlisted Supply: {token.remainingSupply}</p>
+              <p>Quantity For Sale: {token.listedForSale}</p>
+              <p>Unlisted Quantity: {token.remainingQuanity}</p>
+              <button onClick={() => setEditingTokenId(token.tokenId)}>
+                Edit
+              </button>
+              {editingTokenId === token.tokenId && (
+                <ManageToken
+                  token={token}
+                  onSave={handleSave}
+                />
+              )}
             </div>
           )}
         </div>
