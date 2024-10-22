e3e7bab910d2121d8b4e3bbd770e5afecd7efcd8
Christian Lopez <chrisdev92@outlook.com>
2024-10-22 19:09:20 +0200

    search functionality and hide/remove option to decide later

--- src/pages/TokenDetail.jsx
+++ src/pages/TokenDetail.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import tokenData from '../data/tokenData'; 
-import ManageToken from '../components/ManageToken';
+import EditTokenData from '../components/EditTokenData';
+import SearchTokenBar from '../components/SearchTokenBar';
 
 const TokenDetail = () => {
   const [tokens, setTokens] = useState(tokenData);
   const[editingTokenId, setEditingTokenId] = useState(null);
   const [expandedTokenId, setExpandedTokenId] = useState(null);
-  const [removeTokenId, setRemoveTokenId] = useState(null);
+  const [hideTokenId, setHideTokenId] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
   //Toggle the token details visibility, if open close it and vice versa
   const toggleDetails = (tokenId) => {
@@ -21,62 +23,71 @@ const TokenDetail = () => {
         token.tokenId === updatedToken.tokenId ? updatedToken : token
       )
     );
-    console.log("tokens:", tokens);
-    
+
     setEditingTokenId(null);
   };
 
   //Remove the token from the list based on the tokenId
-  const handleRemove =(tokenId) => {
-    const confirmRemove = window.confirm("Are you sure you want to remove this token?");
-    if(confirmRemove){
-      setRemoveTokenId(tokenId);
+  const hideHandler =(tokenId) => {
+    const hideConfirm = window.confirm("Are you sure you want to hide this token?");
+    if(hideConfirm){
+      setHideTokenId(tokenId);
       setTokens(tokens.filter((token) => token.tokenId !== tokenId));
     }
    
   }
 
+   const filteredTokens = tokens.filter((token) =>
+     token.name.toLowerCase().includes(searchQuery.toLowerCase())
+   );
+
   return (
-    <div className="tokenList">
-      {tokens.map((token) => (
-        <div
-          key={token.tokenId}
-          className="tokenContainer"
-        >
-          <h2 onClick={() => toggleDetails(token.tokenId)}>{token.name}</h2>
-          {expandedTokenId === token.tokenId && (
-            <div className="tokenDetails">
-              <div className="tokenImgContent">
-                <img
-                  src={token.image}
-                  alt={token.name}
-                />
+    <>
+      <SearchTokenBar
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <div className="tokenList">
+        {filteredTokens.map((token) => (
+          <div
+            key={token.tokenId}
+            className="tokenContainer"
+          >
+            <h2 onClick={() => toggleDetails(token.tokenId)}>{token.name}</h2>
+            {expandedTokenId === token.tokenId && (
+              <div className="tokenDetails">
+                <div className="tokenImgContent">
+                  <img
+                    src={token.image}
+                    alt={token.name}
+                  />
+                </div>
+                <p>Token_Id: {token.tokenId}</p>
+                <p>Name: {token.name}</p>
+                <p>Symbol: {token.symbol}</p>
+                <p>Quantity: {token.quantity}</p>
+                <p>Price: {token.price}</p>
+                <p>Currency: {token.currency}</p>
+                <p>Quantity For Sale: {token.listedForSale}</p>
+                <p>Unlisted Quantity: {token.remainingQuanity}</p>
+                <button onClick={() => setEditingTokenId(token.tokenId)}>
+                  Edit
+                </button>
+                {editingTokenId === token.tokenId && (
+                  <EditTokenData
+                    token={token}
+                    onSave={handleSave}
+                  />
+                )}
+                <button onClick={() => hideHandler(token.tokenId)}>
+                  Hide token
+                </button>
               </div>
-              <p>Token_Id: {token.tokenId}</p>
-              <p>Name: {token.name}</p>
-              <p>Symbol: {token.symbol}</p>
-              <p>Quantity: {token.quantity}</p>
-              <p>Price: {token.price}</p>
-              <p>Currency: {token.currency}</p>
-              <p>Quantity For Sale: {token.listedForSale}</p>
-              <p>Unlisted Quantity: {token.remainingQuanity}</p>
-              <button onClick={() => setEditingTokenId(token.tokenId)}>
-                Edit
-              </button>
-              {editingTokenId === token.tokenId && (
-                <ManageToken
-                  token={token}
-                  onSave={handleSave}
-                />
-              )}
-              <button onClick={() => handleRemove(token.tokenId)}>
-                Remove
-              </button>
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
