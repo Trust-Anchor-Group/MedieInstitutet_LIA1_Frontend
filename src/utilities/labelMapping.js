4780df61d18acd2b6176a980fafde0602c9479b3
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-11-08 14:27:35 +0100

    :sparkles: add label mapping for dynamic form attributes

---
+++ src/utilities/labelMapping.js
@@ -0,0 +1,11 @@
+export default {
+    FIRST: 'First name',
+    LAST: 'Last name',
+    ADDR: 'Address',
+    ZIP: 'ZIP code',
+    CITY: 'City',
+    COUNTRY: 'Country',
+    EMAIL: 'Email',
+    AGENT: 'Agent',
+    JID: 'Jid'
+};
