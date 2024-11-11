51c66c68db985b0b1cf7130a65e7a0dd18cff297
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2024-11-11 19:36:23 +0100

    :zap: update: image reference

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Banner } from '../components/Banner';
+import promoImage from '/public/assets/images/hero.jpg';
 
 const HomePage = () => {
   return (
     <>
-      <Banner imageSource="/public/assets/images/hero.jpg">
+      <Banner imageSource={promoImage}>
         <div className="banner__promo-card">
           <h1>
             Forget about programmers — creating contracts has never been easier!
